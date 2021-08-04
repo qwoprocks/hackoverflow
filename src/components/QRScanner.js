e1c4760e0b60a39e8b062f7795537f9591300e78
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2021-08-04 23:51:32 +0800

    Fix camera distortion on Android

--- src/components/QRScanner.js
+++ src/components/QRScanner.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, TouchableOpacity, Platform } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import { Camera } from 'expo-camera';
+import CameraFullScreen from './CameraFullScreen';
 import QrReader from 'react-qr-reader'
 import { Auth } from 'aws-amplify'
 import { DataStore } from "@aws-amplify/datastore"
@@ -9,10 +9,9 @@ import { StoreProfile, GiftVoucher, UserVoucher, UserProfile } from '../models';
 import { useIsFocused } from "@react-navigation/native";
 
 export default function QRScanner({ navigation }) {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [aspectRatio, setAspectRatio] = useState('4:3')
   const [isStore, setIsStore] = useState(false)
-  const isFocused = useIsFocused();
+  const isFocused = useIsFocused()
 
   useEffect(() => {
     (async () => {
@@ -25,12 +24,7 @@ export default function QRScanner({ navigation }) {
         setIsStore(false)
       }
     })();
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
   }, [])
-  
 
   const handleBarCodeScanned = async (data) => {
     if (!data) return
@@ -82,73 +76,45 @@ export default function QRScanner({ navigation }) {
     alert('Error occurred')
   }
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
   return (
-    <View style={styles.container}>
+    <>
       {
         Platform.OS === 'web' ? (
-          isFocused && <QrReader
-            delay={100}
-            onError={handleBarCodeError}
-            onScan={handleBarCodeScanned}
-            style={{ display: 'flex', alignSelf: 'center', width: 'min(100vw, 100vh)', height: 'min(100vw, 100vh)' }}
-          />
-        ) : (  
-          isFocused && <Camera
-            style={styles.camera}
-            type={type}
-            onBarCodeScanned={(res) => handleBarCodeScanned(res.data)}
-            barCodeScannerSettings={{
-              barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
-            }}
-          >
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                  setType(
-                    type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back
-                  );
-                }}>
-                <Text style={styles.text}> Flip </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
+          <View style={styles.webcontainer}>
+            {
+              isFocused && <QrReader
+                delay={100}
+                onError={handleBarCodeError}
+                onScan={handleBarCodeScanned}
+                style={{ display: 'flex', alignSelf: 'center', width: 'min(100vw, 100vh)', height: 'min(100vw, 100vh)' }}
+              />
+            }
+          </View>
+        ) : (
+          <View style={styles.appcontainer}>
+            {
+              isFocused && <CameraFullScreen
+                onBarCodeScanned={(res) => handleBarCodeScanned(res.data)}
+                barCodeScannerSettings={{
+                  barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr],
+                }}
+              >
+              </CameraFullScreen>
+            }
+          </View>
         )
       }
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  webcontainer: {
     flex: 1,
     alignContent: 'center',
     justifyContent: 'center'
   },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
+  appcontainer: {
+    flex: 1
   },
 });
